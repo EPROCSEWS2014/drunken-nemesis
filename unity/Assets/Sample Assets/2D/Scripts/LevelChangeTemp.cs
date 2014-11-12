ac801bfa223507265036e769583d469904811c93
Beeti <a.steinbach86@yahoo.de>
2014-11-12 22:04:17 +0100

    Levelwechsel durch eine gelbe Tür mit Enter!

---
+++ unity/Assets/Sample Assets/2D/Scripts/LevelChangeTemp.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelChangeTemp : MonoBehaviour {
+	public string LeveltoChange; //String to choose the Level you want to teleport to
+
+	void OnTriggerStay2D(Collider2D touchSensor) //Need to check, who is in front of 
+	{ 
+		if(touchSensor.tag=="Player") //If the "Player" is in front of..
+		{ 
+			if (Input.GetKeyDown(KeyCode.Return)) //.. check if return is pressed to..
+			{
+				Application.LoadLevel(LeveltoChange); //.. change the Level to "LeveltoChange". Name "LeveltoChange" in Unity.
+			}
+		}
+	}
+}
