747fa5f69fd37779e2993602fce15f0eb334381c
Dennis <dennis.violante@yahoo.de>
2014-11-16 21:14:19 +0100

    Bugchecking FFA!

--- unity/Assets/Scripts/PlayerScript/PlayerLogic.cs
+++ unity/Assets/Scripts/PlayerScript/PlayerLogic.cs
@@ -5,18 +5,18 @@ using System.Collections;
 public class PlayerLogic : MonoBehaviour {
 
 	public Transform[] thingsThatMakePanic;
-	float[] distance;
 	public Transform player;
 	public int triggerDistance;
 
+	float[] distance;
+	int panikLevel = 100;
+	int panikLevelmax = 100;
+	bool istNaheMonster;
+
 	// Use this for initialization
 	void Start () {
-<<<<<<< HEAD
-		int panikLevel = 100;
-		int panikLevel_max = 100;
-		boolean istNaheMonster;
-	
-=======
+
+
 
 		if(player == null) {
 			Debug.LogError("Please assign a player object");
@@ -26,67 +26,71 @@ public class PlayerLogic : MonoBehaviour {
 		}
 		distance = new float[thingsThatMakePanic.Length];
 
->>>>>>> origin/master
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-<<<<<<< HEAD
+		for(int i = 0; i < thingsThatMakePanic.Length; i++){
+			distance[i] = Vector2.Distance(thingsThatMakePanic[i],player);
+			
+			
+			if(distance[i] <= triggerDistance){
+				
+				istNaheMonster = true;
+				
+			}else if(i+1 == thingsThatMakePanic.Length){
+				
+				istNaheMonster = false;
+			}
 
 
 	
 	}
-    int doPanic (int height){
-				if (istNaheMonster == true) {
-						panikLevel = panikLevel - height;
-
-				} else {
-						if (panikLevel == panikLevel_max) {
-								Debug.Log ("Vitality Full");
-			            
-						} else {
-								panikLevel = panikLevel + height;
-				     
-				            
-			            
-						}
-				}
+  
 		}
-		switch (panikLevel){
-			case 100 : //Keine Änderung
 
-			case 50 : //Veränderte Sicht , Blur
-
-			case 40 : //
-
-			case 30 : //
-
-			case 20 : //
-
-			case 10 : //
-
-			case 0 : //
-
-		}
-=======
-		for(int i = 0; i < thingsThatMakePanic.Length; i++){
-			distance[i] = Vector2.Distance(thingsThatMakePanic[i],player);
-
-
-			if(distance[i] <= triggerDistance){
-
-				istNaheMonster = true;
-
-			}else if(i+1 == thingsThatMakePanic.Length){
-
-				istNaheMonster = false;
+	int doPanic (int height){
+		if (istNaheMonster == true) {
+			panikLevel = panikLevel - height;
+			
+		} else {
+			if (panikLevel == panikLevelmax) {
+				Debug.Log("Vitality Full");
+				
+			} else {
+				panikLevel = panikLevel + height;
+				
+				
+				
 			}
+		}
+
+		switch (panikLevel){
+		case 100 : //Keine Änderung
+			break;
+		case 50 : //Veränderte Sicht , Blur
+			break;
+		case 40 : //
+			break;
+		case 30 : //
+			break;
+		case 20 : //
+			break;
+		case 10 : //
+			break;
+		case 0 : //
+			break;
+		}
+
+
+
 
 		}
 
 	}
 
->>>>>>> origin/master
 
 
-}
+
+
