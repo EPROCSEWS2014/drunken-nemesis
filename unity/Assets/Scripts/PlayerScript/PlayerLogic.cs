04c62e15e4279e634ad60c4123aa3192058fea6b
Dennis <dennis.violante@yahoo.de>
2014-11-16 20:56:43 +0100

    Adding a simple Algorithm for Monster Scanning
    I addet a simple script if the monster(s) are within a trigger range, so
    that it will drain sanity.

--- unity/Assets/Scripts/PlayerScript/PlayerLogic.cs
+++ unity/Assets/Scripts/PlayerScript/PlayerLogic.cs
@@ -3,16 +3,43 @@ using System.Collections;
 
 public class PlayerLogic : MonoBehaviour {
 
-	public Transform[] testing;
-
+	public Transform[] thingsThatMakePanic;
+	float[] distance;
+	public Transform player;
+	public int triggerDistance;
 
 	// Use this for initialization
 	void Start () {
-	
+
+		if(player == null) {
+			Debug.LogError("Please assign a player object");
+
+		}else if(thingsThatMakePanic[0] == null){
+			Debug.LogError("Please assign at least a enemy object");
+		}
+		distance = new float[thingsThatMakePanic.Length];
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		for(int i = 0; i < thingsThatMakePanic.Length; i++){
+			distance[i] = Vector2.Distance(thingsThatMakePanic[i],player);
+
+
+			if(distance[i] <= triggerDistance){
+
+				istNaheMonster = true;
+
+			}else if(i+1 == thingsThatMakePanic.Length){
+
+				istNaheMonster = false;
+			}
+
+		}
+
 	}
+
+
+
 }
