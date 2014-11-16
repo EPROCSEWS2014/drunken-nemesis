09aaeaecf6e03749703a6c37f1a5d1af71f38ea2
Dr. Ogen <marc.schnitzler@smail.inf.h-brs.de>
2014-11-16 20:58:15 +0100

    Test2

--- unity/Assets/Scripts/PlayerScript/PlayerLogic.cs
+++ unity/Assets/Scripts/PlayerScript/PlayerLogic.cs
@@ -8,11 +8,50 @@ public class PlayerLogic : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+		int panikLevel = 100;
+		int panikLevel_max = 100;
+		boolean istNaheMonster;
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+
 	
 	}
+    int doPanic (int height){
+				if (istNaheMonster == true) {
+						panikLevel = panikLevel - height;
+
+				} else {
+						if (panikLevel == panikLevel_max) {
+								Debug.Log ("Vitality Full");
+			            
+						} else {
+								panikLevel = panikLevel + height;
+				     
+				            
+			            
+						}
+				}
+		}
+		switch (panikLevel){
+			case 100 : //Keine Änderung
+
+			case 50 : //Veränderte Sicht , Blur
+
+			case 40 : //
+
+			case 30 : //
+
+			case 20 : //
+
+			case 10 : //
+
+			case 0 : //
+
+		}
+
+
 }
