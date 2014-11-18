13a67b40b0594da808d8404df97d26feb43eb182
Beeti <a.steinbach86@yahoo.de>
2014-11-18 21:12:16 +0100

    Fade in/out added
    - Add 2D -> Prefabs -> FadeInOut to the scene

---
+++ unity/Assets/EPRO_made_Scripts/TransitionScript/FadeInOut.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FadeInOut : MonoBehaviour 
+{
+	public float fadeSpeed = 0.05f; //The fade in and out speed. Can be changed in Unity.
+	public static bool sceneStarting = true; //Bool to start fade in (true) or out (false).
+	
+	void Awake()
+	{
+		guiTexture.pixelInset = new Rect (0f, 0f, Screen.width, Screen.height);
+	}
+
+	void Update()
+	{
+		if (sceneStarting) // Check if the scene is starting and..
+		{
+			guiTexture.color = Color.Lerp (guiTexture.color, Color.clear, fadeSpeed); 
+			if (guiTexture.color.a <= 0.0f) 
+			{
+				guiTexture.color = Color.clear;
+				guiTexture.enabled = false;
+			}
+		} 
+
+		if (!sceneStarting)
+		{
+			guiTexture.enabled = true;
+			guiTexture.color = Color.Lerp (guiTexture.color, Color.black, fadeSpeed);
+			if (guiTexture.color.a >= 0.40f)
+			{
+				sceneStarting = true;
+			}
+		}
+	}
+}
--- unity/Assets/EPRO_made_Scripts/TransitionScript/LevelChangeTemp.cs
+++ unity/Assets/EPRO_made_Scripts/TransitionScript/LevelChangeTemp.cs
@@ -5,6 +5,7 @@ public class LevelChangeTemp : MonoBehaviour {
 	public string LevelToChange; //String to choose the Level you want to teleport to
 	public string LoadLevelOnTouch; //String to teleport with a simple touch
 	public string LoadLevelOnReturn; // String to teleport with the return-button
+	
 
 	void OnTriggerStay2D(Collider2D touchSensor) //Need to check, who is in front of 
 	{ 
@@ -12,11 +13,13 @@ public class LevelChangeTemp : MonoBehaviour {
 		{ 
 			if (LoadLevelOnReturn!="" && Input.GetKeyDown(KeyCode.Return)) //.. check if string LoadLevelOnReturn is not empty and return is pressed to..
 			{
+				FadeInOut.sceneStarting = false; // set sceneStarting to false to fade out
 				Application.LoadLevel(LevelToChange); //.. change the Level to "LeveltoChange". Name "LeveltoChange" in Unity.
 			}
 
 			if (LoadLevelOnTouch!="") //Check if string LoadLevelOnTouch is not empty to..
 			{
+				FadeInOut.sceneStarting = false; // set sceneStarting to false to fade out
 				Application.LoadLevel(LevelToChange); //.. teleport on a simple touch like a warpgate
 			}
 		}
