9cedc83a4f95c34fc990d5aff30f5802dec455a0
Beeti <a.steinbach86@yahoo.de>
2014-11-18 21:26:12 +0100

    Add some comments to FadeInOut.cs

--- unity/Assets/EPRO_made_Scripts/TransitionScript/FadeInOut.cs
+++ unity/Assets/EPRO_made_Scripts/TransitionScript/FadeInOut.cs
@@ -15,21 +15,21 @@ public class FadeInOut : MonoBehaviour
 	{
 		if (sceneStarting) // Check if the scene is starting and..
 		{
-			guiTexture.color = Color.Lerp (guiTexture.color, Color.clear, fadeSpeed); 
-			if (guiTexture.color.a <= 0.0f) 
+			guiTexture.color = Color.Lerp (guiTexture.color, Color.clear, fadeSpeed); // Set up the transparency..
+			if (guiTexture.color.a <= 0.0f) //.. until 0%,..
 			{
-				guiTexture.color = Color.clear;
-				guiTexture.enabled = false;
+				guiTexture.color = Color.clear; //.. clear the color and..
+				guiTexture.enabled = false; //.. disable the fader.png.
 			}
 		} 
 
-		if (!sceneStarting)
+		if (!sceneStarting) // Check if the scene is ending and..
 		{
-			guiTexture.enabled = true;
-			guiTexture.color = Color.Lerp (guiTexture.color, Color.black, fadeSpeed);
-			if (guiTexture.color.a >= 0.40f)
+			guiTexture.enabled = true; // .. enable the fader.png
+			guiTexture.color = Color.Lerp (guiTexture.color, Color.black, fadeSpeed); // Set down the transparency..
+			if (guiTexture.color.a >= 0.40f) //.. until 40% and..
 			{
-				sceneStarting = true;
+				sceneStarting = true; //.. set sceneStarting to true to set up the transparency.
 			}
 		}
 	}
