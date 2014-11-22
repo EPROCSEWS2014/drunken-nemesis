061a2594e78cac978da0f978b2dce23eeb0c40ad
Dennis <dennis.violante@yahoo.de>
2014-11-22 17:41:36 +0100

    Better PlayerLogic /Minor Changes to Camera(Rearranging)
    I changed the script, so that the script automatically gets the
    Transforms from the scenery. Also sanity will now drain, if it's in the
    trigger radius of the "Boxmonster"
    
    @white-Crayon  You could also implement your monster now there, just
    sayin'
    
    I also rearranged the cam a bit, so it looks much better with this
    background.

--- unity/Assets/EPRO_made_Scripts/PlayerScript/PlayerLogic.cs
+++ unity/Assets/EPRO_made_Scripts/PlayerScript/PlayerLogic.cs
@@ -3,31 +3,46 @@ using System.Collections;
 
 
 public class PlayerLogic : MonoBehaviour {
-	
-	public Transform thingsThatMakePanic;
-	public Transform player;
-	public int triggerDistance = 5;
-	public int panicCap = 100;
-	
-	int panikLevel;
 
-	float distance;
-	bool istNaheMonster;
-	
+	private Transform player; // The player object 
+
+
+	//Everything relating to the panicMethods
+
+		private float 	sanityLevel;  // The sanity level
+		public 	int 	maximumSanity = 1000;	
+		public 	int 	minimumSanity = 0;
+		public 	float 	sanityDrain = 100;
+		private	float 	sanityRegen;
+		public	int 	divisor = 10;
+		private	bool 	isNearToMonster;
+			
+		//Attributes only concerning the EnemyFinding Algorithm
+			private	Transform 	thingsThatMakePanic; 	 	// A tranform Father, which contains all the monsters
+			public 	int 		triggerDistance = 5;		// The Distance where the Algorithm searches for Enemies
+			private float 		distance;					// The calculated distance to the monster
+
+		//End Algorithm
+
+	//End panicMethods
+
+
+
 	// Use this for initialization
 	void Start () {
-		panikLevel = panicCap;
+		this.sanityLevel = this.maximumSanity; 
+		this.sanityRegen = (sanityDrain/divisor); // Calculates the Regeneration of the Sanity
+		this.player = this.transform; // Returns the Player Transform
+		this.thingsThatMakePanic = GameObject.FindWithTag("EnemyC").transform;
+
+
 		if(player == null) {
-			Debug.LogError("Please assign a player object");
-			
-		}else if(thingsThatMakePanic.GetChild(0) == null){
-			Debug.LogError("Please assign at least an enemy object");
-		}else{
-			
-			
-			
-		}
-		
+				Debug.LogError("Please assign a player object");
+				
+		}			else if(thingsThatMakePanic.GetChild(0) == null){
+						Debug.LogError("Please assign at least an enemy object");
+							}
+
 		
 	}
 	
@@ -35,30 +50,45 @@ public class PlayerLogic : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		for(int i = 0; i < thingsThatMakePanic.childCount; i++){
+
+		detectionAlgorithm();
 		
-			distance = Vector2.Distance(makeVector3To2(thingsThatMakePanic.GetChild(i)),makeVector3To2(player));
-			//Debug.Log("Distance of Index " + i + " " + distance);
-			if(distance < triggerDistance){
-
-			istNaheMonster = true;
-				break;
-
-				//Debug.Log(istNaheMonster);
-				
-			}else{
-				istNaheMonster = false;
-			}
-			
-		}
-
-		Debug.Log (istNaheMonster);
-	
 		
 		
 	}
-	
-	Vector2 makeVector3To2(Transform t){
+
+	private void detectionAlgorithm(){
+
+		
+		for(int i = 0; i < this.thingsThatMakePanic.childCount; i++){
+			
+			this.distance = Vector2.Distance(makeVector3To2(thingsThatMakePanic.GetChild(i)),makeVector3To2(player));
+			
+			if(distance < triggerDistance){
+				
+				this.isNearToMonster = true;
+				break;
+				
+				
+			}else{
+				this.isNearToMonster = false;
+			}
+			
+		}
+		
+		if(isNearToMonster){
+			this.decreaseSanity(sanityDrain);
+		}else{
+			
+			this.regainSanity(sanityRegen);
+		}
+
+	}
+
+	/**
+	Transforms a Vector3 into a Vector2
+	 */
+	public Vector2 makeVector3To2(Transform t){
 	
 	
 			return new Vector2(t.position.x,t.position.y);
@@ -68,21 +98,23 @@ public class PlayerLogic : MonoBehaviour {
 		
 		
 		
-		void decreaseSanity (int height){
-		bool maxCapReached = (panikLevel - height > 100);
-		bool minCapReached = (panikLevel - height < 0);
-		if (!(maxCapReached | minCapReached)) {
-					
-						panikLevel = panikLevel - height;
-				} else if (maxCapReached) {
-			panikLevel = 100;
+		public void decreaseSanity (float height){
+		bool minCapReached = ((sanityLevel - height) < minimumSanity);
+		Debug.Log(sanityLevel);
+		if (minCapReached) {
+			this.sanityLevel = minimumSanity;}
+		else{
 
-				} else if (minCapReached) {
-			panikLevel = 0;
-				}
-			if (panikLevel == 0) {
+			this.sanityLevel -= height;
+
+		}
+
+	//	Debug.Log(panicLevel);
+
+
+			if (sanityLevel == minimumSanity) {
 				Debug.Log ("PANIK!!!");
-		} else if (panikLevel == 100) {
+		} else if (sanityLevel == maximumSanity) {
 				
 					Debug.Log ("Vollständige Geistige Gesundheit!");
 
@@ -92,7 +124,7 @@ public class PlayerLogic : MonoBehaviour {
 			
 			
 			
-			switch (panikLevel) {
+			switch (Mathf.RoundToInt(sanityLevel)) {
 			case 50: //Veränderte Sicht , Blur
 				break;
 			case 40: //Einschränkungen in der Bewegung
@@ -110,9 +142,28 @@ public class PlayerLogic : MonoBehaviour {
 		
 		
 		
-		void regainSanity (int height){
-			decreaseSanity(-height);
+		public void regainSanity (float height){
+		bool maxCapReached = ((sanityLevel + height) > maximumSanity);
+		if(maxCapReached){
+
+			this.sanityLevel = this.maximumSanity;
+
+		}else{
+			this.decreaseSanity(-height);
+
 		}
+		}
+
+	public float getPanicLevel(){
+
+		return this.sanityLevel;
+	}
+	public void setPanicLevel(float newPanicLevel){
+
+		this.sanityLevel = newPanicLevel;
+
+
+	}
 		
 	}
 	
