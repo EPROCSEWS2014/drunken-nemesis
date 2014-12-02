1906c1f268f7ef5c19173bebfcf833d8aa3f90c7
mavogl <marvin_vogl@hotmail.de>
2014-12-02 19:14:55 +0100

    Licht
    Kleinere Fehler wurden behoben

--- unity/Assets/LightCameraFollow.cs
+++ unity/Assets/LightCameraFollow.cs
@@ -8,44 +8,45 @@ public class LightCameraFollow : MonoBehaviour {
 	private Transform player;
 	private bool boolean = true;
 
-
-	void Awake () {
+	void Awake() {
 		player = GameObject.FindGameObjectWithTag("Player").transform;
 	}
 	
-	
-	void Update () {
+	void Update() {
 		Track();
 	}
 
-	bool CheckRight () {
-		return Input.GetKeyDown (KeyCode.D) && !boolean;
-	}
-	bool CheckLeft () {
-		return Input.GetKeyDown (KeyCode.A) && boolean;
+
+	bool CheckRight() {
+		return !(Input.GetKey (KeyCode.A));
 	}
 
-	void Track () {
+	bool CheckLeft() {
+		return !(Input.GetKey (KeyCode.D));
+	}
+
+	void Track() {
 		float targetX = player.position.x + positionX;
 		float targetY = player.position.y - positionY;
 		float scale = player.localScale.x;
 		Vector3 vectora = new Vector3 (0, 240, 0);
 		Vector3 vectorb = new Vector3 (0, 120, 0);
 
-		if (CheckRight ()) {
-			transform.rotation = new Quaternion(0,0.5f,0,0);
+        if (CheckLeft() && !boolean) {
+            transform.rotation = new Quaternion(0, 0.5f, 0, 0);
+            transform.Rotate(vectorb);
+            positionX = 2;
+        } else {
+            boolean = true;
+        }
+		if (CheckRight () && boolean) {
+			transform.rotation = new Quaternion(0, 0.5f, 0, 0);
 			transform.Rotate(vectora);
 			positionX = -2;
-			boolean = true;
-		} 
-		if (CheckLeft ()) {
-			transform.rotation = new Quaternion(0,0.5f,0,0);;
-			transform.Rotate(vectorb);
-			positionX = 2;
-			boolean = false;
-		}
+		} else {
+            boolean = false;
+        }
 
 		transform.position = new Vector3(targetX, targetY, transform.position.z);
 	}
-
 }
