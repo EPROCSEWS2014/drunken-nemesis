7fdec6e954cfecd4ba3fd7caf91897f11fb2f96b
white-Crayon <ben.kirsche.95@googlemail.com>
2014-12-02 11:02:12 +0100

    RestartScript_v2
    Bei der Kollision von Monster und Spieler wird das RestartScript
    ausgeführt.
    
    Dort hat(soll) jeder Gruppe ihre eigene Methode.
    Setzt da bitte alle Werte für ein neues Spiel zurück.

--- unity/Assets/EPRO_made_Scripts/MonsterScript/MonsterController.cs
+++ unity/Assets/EPRO_made_Scripts/MonsterScript/MonsterController.cs
@@ -2,66 +2,73 @@
 
 public class MonsterController : MonoBehaviour
 {
-		public float speed = 10;
-		Vector2 playerPosition;
-		float distance;
-		LayerMask whatIsGround;
-		Transform groundCheck;
-		float groundedRadius = .2f;
-		bool grounded = false;
-		bool facingRight = true;
-		Transform ceilingCheck;
-		float ceilingRadius = .01f;
-		Animator anim;
-		RestartScript restart;
+    public float speed = 10;
+    Vector2 playerPosition;
+    float distance;
+    LayerMask whatIsGround;
+    Transform groundCheck;
+    float groundedRadius = .2f;
+    bool grounded = false;
+    bool facingRight = true;
+    Transform ceilingCheck;
+    float ceilingRadius = .01f;
+    Animator anim;
+    RestartScript restart;
 
-		void Awake ()
-		{
-				groundCheck = transform.Find ("GroundCheck");
-				ceilingCheck = transform.Find ("CeilingCheck");
-				anim = GetComponent<Animator> ();
-				restart = GetComponent<RestartScript> ();
-		}
-	
-		void Update ()
-		{
-				playerPosition = GameObject.Find ("2D Character").transform.position;
-				distance = playerPosition.x - transform.position.x;
-				if (distance == 0) {
-						restart.Restart ();
-			Debug.Log("distance 0");
-				} else if (distance > 0) {
-						rigidbody2D.velocity = new Vector2 (speed, rigidbody2D.velocity.y);
-						if (!facingRight)
-								Flip ();
-				} else {
-						rigidbody2D.velocity = new Vector2 (-speed, rigidbody2D.velocity.y);
-						if (facingRight)
-								Flip ();
-				}
-				anim.SetFloat ("Speed", speed);
+    void Awake()
+    {
+        groundCheck = transform.Find("GroundCheck");
+        ceilingCheck = transform.Find("CeilingCheck");
+        anim = GetComponent<Animator>();
+        restart = GetComponent<RestartScript>();
+    }
+    
+    void Update()
+    {
+        playerPosition = GameObject.Find("2D Character").transform.position;
+        distance = playerPosition.x - transform.position.x;
+        if (distance > 0)
+        {
+            rigidbody2D.velocity = new Vector2(speed, rigidbody2D.velocity.y);
+            if (!facingRight)
+                Flip();
+        } else
+        {
+            rigidbody2D.velocity = new Vector2(-speed, rigidbody2D.velocity.y);
+            if (facingRight)
+                Flip();
+        }
+        anim.SetFloat("Speed", speed);
 
-		}
-	
-		void FixedUpdate ()
-		{
-				grounded = Physics2D.OverlapCircle (groundCheck.position, groundedRadius, whatIsGround);
-				anim.SetBool ("Ground", grounded);
-				anim.SetFloat ("vSpeed", rigidbody2D.velocity.y);
-		}
+    }
+    
+    void FixedUpdate()
+    {
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundedRadius, whatIsGround);
+        anim.SetBool("Ground", grounded);
+        anim.SetFloat("vSpeed", rigidbody2D.velocity.y);
+    }
 
-		void Flip ()
-		{
-				facingRight = !facingRight;
+    void Flip()
+    {
+        facingRight = !facingRight;
 
-				Vector3 theScale = transform.localScale;
-				theScale.x *= -1;
-				transform.localScale = theScale;
-		}
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
 
-		void Spawn (Vector2 Position)
-		{
-				gameObject.SetActive (true);
-				transform.position = Position;
-		}
+    void Spawn(Vector2 Position)
+    {
+        gameObject.SetActive(true);
+        transform.position = Position;
+    }
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.name == "2D Character")
+        {
+            restart.Restart();
+        }
+    }
 }
--- unity/Assets/EPRO_made_Scripts/TransitionScript/RestartScript.cs
+++ unity/Assets/EPRO_made_Scripts/TransitionScript/RestartScript.cs
@@ -1,26 +1,30 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class RestartScript : MonoBehaviour {
+public class RestartScript : MonoBehaviour
+{
+    GameObject Monster;
+    GameObject Player;
 
-	GameObject Monster;
-	GameObject Player;
-	void Start () {
-	
-	}
+    void Start()
+    {
+        Monster = GameObject.Find("Monster");
+        Player = GameObject.Find("2D Character");
+    }
 
-	public void Restart(){
-		RestartMonster ();
-		RestartPlayer();
-	}
+    public void Restart()
+    {
+        RestartMonster();
+        RestartPlayer();
+    }
 
-	public void RestartMonster(){
-		Monster = GameObject.Find ("Monster");
-		Monster.SetActive (false);
-	}
+    public void RestartMonster()
+    {
+        Monster.SetActive(false);
+    }
 
-	public void RestartPlayer(){
-		Player = GameObject.Find ("2D Character");
-		Player.transform.position = new Vector2 (0, 0);
-	}
+    public void RestartPlayer()
+    {
+        Player.transform.position = new Vector2(0, 0);
+    }
 }
