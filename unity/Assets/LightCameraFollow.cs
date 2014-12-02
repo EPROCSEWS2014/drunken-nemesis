a16b14fda2345c3ee30596d20aaebd51a2031444
mavogl <marvin_vogl@hotmail.de>
2014-12-02 09:37:37 +0100

    Licht
    Fehler leider immer noch nicht gefunden warum das Licht nicht rotiert.
    Wenn jemand im ersten Level Licht braucht einfach das Temporary Light
    anmachen!

---
+++ unity/Assets/LightCameraFollow.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightCameraFollow : MonoBehaviour {
+
+	public float xPosition = -2;
+	private Transform player;
+	private Transform rota;
+	private Transform rotb;
+
+
+	void Awake () {
+		player = GameObject.FindGameObjectWithTag("Player").transform;
+		rota = GameObject.FindGameObjectWithTag("rota").transform;
+		rotb = GameObject.FindGameObjectWithTag("rotb").transform;
+	}
+	
+	
+	void Update () {
+		Track();
+		float scale = player.localScale.x;
+	}
+
+	void Track () {
+		float targetX = player.position.x + xPosition;
+		float targetY = player.position.y;
+
+		float scale = player.localScale.x;
+
+		if (scale > 0) {
+			transform.rotation = rota.rotation;
+			xPosition = -2;
+		} 
+		else {
+			transform.rotation = rotb.rotation;
+			xPosition = 2;
+		}
+
+		transform.position = new Vector3(targetX, targetY, transform.position.z);
+	}
+
+}
