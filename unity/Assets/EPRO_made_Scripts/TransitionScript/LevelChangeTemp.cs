5aa7b042415b7f990e61ca52f632af9319a2069b
Beeti <a.steinbach86@yahoo.de>
2014-12-08 20:01:36 +0100

    Transition update (not 100%)

--- unity/Assets/EPRO_made_Scripts/TransitionScript/LevelChangeTemp.cs
+++ unity/Assets/EPRO_made_Scripts/TransitionScript/LevelChangeTemp.cs
@@ -5,8 +5,30 @@ public class LevelChangeTemp : MonoBehaviour {
 	public string LevelToChange; //String to choose the Level you want to teleport to
 	public string LoadLevelOnTouch; //String to teleport with a simple touch
 	public string LoadLevelOnReturn; // String to teleport with the return-button
+	public string DoorTagToTeleport;
+	public int DoorID;
+	int rDoorID;
+	protected Transform Door;
+	protected Transform player;
 
     public AudioClip doorOpen;
+
+	void Start()
+	{
+		player = GameObject.FindWithTag("Player").transform;
+		Door = GameObject.FindWithTag(DoorTagToTeleport).transform;
+	}
+
+	void Awake()
+	{
+		if (DoorTagToTeleport == DoorTagToTeleport) {
+					Start ();
+					player.transform.position = Door.transform.position;
+		
+				} else {
+					Start ();
+				}
+	}
 
 	void OnTriggerStay2D(Collider2D touchSensor) //Need to check, who is in front of 
 	{ 
@@ -17,6 +39,7 @@ public class LevelChangeTemp : MonoBehaviour {
 				FadeInOut.sceneStarting = false; // set sceneStarting to false to fade out
 				Application.LoadLevel(LevelToChange); //.. change the Level to "LeveltoChange". Name "LeveltoChange" in Unity.
                 AudioSource.PlayClipAtPoint(doorOpen, transform.position, 1f);
+
 			}
 
 			if (LoadLevelOnTouch!="") //Check if string LoadLevelOnTouch is not empty to..
