8a19af4b23e9f358ed659fb37e7922308c6ee08d
Beeti <a.steinbach86@yahoo.de>
2014-12-10 14:50:57 +0100

    Transition Update

--- unity/Assets/EPRO_made_Scripts/TransitionScript/LevelChangeTemp.cs
+++ unity/Assets/EPRO_made_Scripts/TransitionScript/LevelChangeTemp.cs
@@ -6,25 +6,23 @@ public class LevelChangeTemp : MonoBehaviour {
 	public string LoadLevelOnTouch; //String to teleport with a simple touch
 	public string LoadLevelOnReturn; // String to teleport with the return-button
 	public string DoorTagToTeleport;
-	public int DoorID;
-	int rDoorID;
-	protected Transform Door;
-	protected Transform player;
+	static string DoorTagTemp;
+	Transform Door;
+	Transform player;
 
     public AudioClip doorOpen;
 
-	void ReAssign()
-	{
-		player = GameObject.FindWithTag("Player").transform;
-		Door = GameObject.FindWithTag(DoorTagToTeleport).transform;
-	}
-
 	void Awake()
 	{
-		if (DoorTagToTeleport != "") {
-					ReAssign ();
-					player.transform.position = Door.transform.position;
-				}
+		if (DoorTagTemp!=null)
+		{
+			player = GameObject.FindWithTag ("Player").transform;
+			Door = GameObject.FindWithTag (DoorTagTemp).transform;
+			player.transform.position = Door.transform.position;
+		}else{
+			player = GameObject.FindWithTag ("Player").transform;
+			player.transform.position = player.transform.position;
+		}
 	}
 
 	void OnTriggerStay2D(Collider2D touchSensor) //Need to check, who is in front of 
@@ -33,10 +31,11 @@ public class LevelChangeTemp : MonoBehaviour {
 		{ 
 			if (LoadLevelOnReturn!="" && Input.GetKeyDown(KeyCode.Return)) //.. check if string LoadLevelOnReturn is not empty and return is pressed to..
 			{
+				DoorTagTemp = DoorTagToTeleport;
 				FadeInOut.sceneStarting = false; // set sceneStarting to false to fade out
 				Application.LoadLevel(LevelToChange); //.. change the Level to "LeveltoChange". Name "LeveltoChange" in Unity.
                 AudioSource.PlayClipAtPoint(doorOpen, transform.position, 1f);
-
+				//Debug.Log(DoorTagTemp);
 			}
 
 			if (LoadLevelOnTouch!="") //Check if string LoadLevelOnTouch is not empty to..
