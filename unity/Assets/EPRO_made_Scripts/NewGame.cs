2eeac7fe40065c275340922ca039e6fc213ccf1e
Beeti <a.steinbach86@yahoo.de>
2014-12-12 21:58:36 +0100

    add MainMenu with temps

--- unity/Assets/EPRO_made_Scripts/NewGame.cs
+++ unity/Assets/EPRO_made_Scripts/NewGame.cs
@@ -3,20 +3,38 @@ using System.Collections;
 
 public class NewGame : MonoBehaviour {
 
-	//public GameObject player;
-	// Use this for initialization
-	GameObject StartPosition;
-	static int PlayerSet = 0;
+	float fadeSpeed = 0.01f;
+	bool sceneStarting = true;
 
-	void Start () {
-		if (PlayerSet == 0) 
+	void Awake()
+	{
+		guiTexture.pixelInset = new Rect (0f, 0f, Screen.width, Screen.height);
+	}
+
+	void Update () {
+		if (sceneStarting) // Check if the scene is starting and..
 		{
-			Transform player = (Instantiate (Resources.Load ("2DCharacter")) as GameObject).transform;
-			StartPosition = GameObject.Find ("StartPosition");
-			player.transform.position = StartPosition.transform.position;
-			player.transform.position = player.transform.position;
+			guiTexture.color = Color.Lerp (guiTexture.color, Color.clear, fadeSpeed); // Set up the transparency..
+			if (guiTexture.color.a <= 0.0f) //.. until 0%,..
+			{
+				guiTexture.color = Color.clear; //.. clear the color and..
+				guiTexture.enabled = false; //.. disable the fader.png.
+			}
+		} 
 
-			PlayerSet = 1;
+		if (Input.GetKey (KeyCode.Return)) 
+		{
+			sceneStarting = false;
+		}
+
+		if (!sceneStarting) // Check if the scene is ending and..
+		{
+			guiTexture.enabled = true; // .. enable the fader.png
+			guiTexture.color = Color.Lerp (guiTexture.color, Color.black, fadeSpeed); // Set down the transparency..
+			if (guiTexture.color.a >= 0.40f) //.. until 40% and..
+			{
+				Application.LoadLevel("level");
+			}
 		}
 	}
 }
