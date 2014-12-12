bd961392fb5909efb9a75d8d2ff39bf8eef0a626
Dr. Ogen <marc.schnitzler@smail.inf.h-brs.de>
2014-12-12 16:10:22 +0100

    Final Final Update: Stamina
    Transition is finally fixed. Stamina works

--- unity/Assets/EPRO_made_Scripts/PlayerScript/PlayerStamina.cs
+++ unity/Assets/EPRO_made_Scripts/PlayerScript/PlayerStamina.cs
@@ -2,35 +2,29 @@ using UnityEngine;
 using System.Collections;
 
 public class PlayerStamina : MonoBehaviour {
-
-	private float normalSpeed;
-	private float runningSpeed;
-	private float speedUp = 1.5f;
-	private float Stamina;
+	
+	private float Stamina = 100;
 	private float maxStamina = 100;
 	private float minStamina = 1;
-	private float staminaDrain = 10;
+	private float staminaDrain = 0.5f;
 	private float staminaRegenaration;
 	private bool isCharacterRunning;
 
-	private PlatformerCharacter2D pc2d;
 
 	void Awake (){
-				pc2d = GetComponent<PlatformerCharacter2D> ();
-		normalSpeed = pc2d.getWalkSpeed ();
+
 		}
 
 	    public void runningStam () {
 					isCharacterRunning = false;
-					if (Input.GetKeyDown(KeyCode.V)) {
+					if (Input.GetKey(KeyCode.V)) {
 							isCharacterRunning = true;
-							runningSpeed = normalSpeed * speedUp;
 					}
+
 					if (isCharacterRunning == true) {
 							Stamina -= staminaDrain;
-				            normalSpeed = runningSpeed;
-			                
-							if ((Stamina -= staminaDrain) < 1) {
+
+			                if ((Stamina -= staminaDrain) < 1) {
 									Stamina = minStamina;
 							}
 					} else {
