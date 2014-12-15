20082fecc76fd1001d91ab8966fc80225c1e3124
Dennis <dennis.violante@yahoo.de>
2014-12-15 21:29:35 +0100

    Licht 2

--- unity/Assets/EPRO_made_Scripts/LightFollow.cs
+++ unity/Assets/EPRO_made_Scripts/LightFollow.cs
@@ -2,10 +2,12 @@
 using System.Collections;
 
 public class LightFollow : MonoBehaviour {
-    private float positionX = -2;
+    
+	private float positionX = -2;
 	public float positionY;
+
 	private Transform player;
-	private bool boolean = true;
+//	private bool boolean = true;
     private Transform rota;
     private Transform rotb;
 
@@ -17,25 +19,29 @@ public class LightFollow : MonoBehaviour {
 	
 	void Update() {
 		Track();
+
 	}
 
 
 	bool CheckRight() {
-		return !(Input.GetKey (KeyCode.A));
+		return (player.localScale.x >= 0);
+		//return !(Input.GetKey (KeyCode.A));
 	}
-
+	/*
 	bool CheckLeft() {
+
 		return !(Input.GetKey (KeyCode.D));
-	}
+	}*/
 
 	void Track() {
 		float targetX = player.position.x + positionX;
 		float targetY = player.position.y - positionY;
-        Vector3 vectora = new Vector3(0, 240, 0);
-		Vector3 vectorb = new Vector3 (0, 120, 0);
+//      Vector3 vectora = new Vector3(0, 240, 0);
+//		Vector3 vectorb = new Vector3 (0, 120, 0);
         Quaternion to;
-        Quaternion from = transform.rotation;
-        if (CheckLeft() && !boolean)
+        Quaternion from;
+        
+	/*	if (!CheckRight())
         {
             to = rotb.rotation;
             //transform.rotation = new Quaternion(0, 0.5f, 0, 0);
@@ -46,18 +52,20 @@ public class LightFollow : MonoBehaviour {
         {
             boolean = true;
             to = rota.rotation;
-        }
-        if (CheckRight() && boolean)
+        }*/
+        if (CheckRight())
         {
             to = rota.rotation;
+			from = rotb.rotation;
             //transform.rotation = new Quaternion(0, 0.5f, 0, 0);
             //transform.Rotate(vectora);
             //positionX = -2;
         }
         else
         {
-            boolean = false;
+           // boolean = false;
             to = rotb.rotation;
+			from = rota.rotation;
         }
 
         transform.rotation = Quaternion.Lerp(from, to, Time.deltaTime * 1);
