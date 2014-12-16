dfdc59ff340c0ef481e49ccee63f80c7aeddf3ab
mavogl <marvin_vogl@hotmail.de>
2014-12-16 22:58:26 +0100

    Letztes Licht cleanup
    Das ist der hoffentlich der letzte Comit fürs Licht.

--- unity/Assets/EPRO_made_Scripts/LightFollow.cs
+++ unity/Assets/EPRO_made_Scripts/LightFollow.cs
@@ -3,14 +3,14 @@ using System.Collections;
 
 public class LightFollow : MonoBehaviour
 {
-	private float lightPositionX = -2;
+	private float lightPositionX = -2.3f;
 
 	private Transform player;
 
     private Transform lightT;
-	private float b = 0.000000001f;
-	private Quaternion leftPos = new Quaternion (0,0.5f,0,1);
-	private Quaternion rightPos = new Quaternion (0,-0.5f,0,1);
+
+	private Quaternion leftPos = new Quaternion (0,0.58f,0,1);
+	private Quaternion rightPos = new Quaternion (0,-0.58f,0,1);
 
 
 	void Awake() 
@@ -42,11 +42,7 @@ public class LightFollow : MonoBehaviour
 			targetX -= lightPositionX;
         }
 
-		if (to.y >= 0 || to.y <= 0)
-        {
-			lightT.rotation = Quaternion.Lerp (lightT.rotation, to, Time.deltaTime * 5);
-		} 
-
+		lightT.rotation = Quaternion.Lerp (lightT.rotation, to, Time.deltaTime * 5);
 		lightT.position = new Vector3(targetX, targetY, transform.position.z);
 	}
 }
