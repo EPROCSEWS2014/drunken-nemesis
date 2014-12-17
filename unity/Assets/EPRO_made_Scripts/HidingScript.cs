2dc22ab3ffc9b5b5ec3d80a16fc6b4d07b81bd79
Beeti <a.steinbach86@yahoo.de>
2014-12-17 22:44:17 +0100

    HidingScript fix

--- unity/Assets/EPRO_made_Scripts/HidingScript.cs
+++ unity/Assets/EPRO_made_Scripts/HidingScript.cs
@@ -7,14 +7,33 @@ public class HidingScript : MonoBehaviour
 	static string ObjectToHideStringTemp;
 	Transform player;
 	Transform ObjectToHide;
-	//int HiddenTrigger = 0;
-	//public BoxCollider2D[] colliderTriggers;
+	Transform Monster;
+	BoxCollider2D colliderTriggerBox;
+	BoxCollider2D EnemyBoxC;
+	CircleCollider2D colliderTriggerCircle;
+	CircleCollider2D EnemyCircleC;
 	private Transform SpotLight;
 	private float speedtemp;
+	bool kleber=false;
+
 
 	void Awake()
 	{
 		SpotLight = GameObject.Find("Spot light").transform;
+	}
+
+	void Update()
+	{
+		if (kleber) 
+		{
+			player.transform.position = ObjectToHide.transform.position;
+		}
+		if(GameObject.FindWithTag("Enemy"))
+		{
+			Monster = GameObject.FindWithTag ("Enemy").transform;
+			EnemyBoxC = Monster.GetComponent<BoxCollider2D>();
+			EnemyCircleC = Monster.GetComponent<CircleCollider2D>();
+		}
 	}
 
 	void OnTriggerStay2D(Collider2D touchSensor) //Need to check, who is in front of 
@@ -24,34 +43,33 @@ public class HidingScript : MonoBehaviour
 			ObjectToHideStringTemp = ObjectToHideString;
 			Debug.Log(ObjectToHideStringTemp);
 			player = GameObject.FindWithTag ("Player").transform;
+
 			ObjectToHide = GameObject.Find(ObjectToHideStringTemp).transform;
-			//colliderTriggers = ObjectToHide.GetComponentsInChildren<BoxCollider2D>();
+			colliderTriggerBox = player.GetComponent<BoxCollider2D>();
+			colliderTriggerCircle = player.GetComponent<CircleCollider2D>();
 			if (Input.GetKeyDown(KeyCode.Return))
 			{
 				switch (PlayerLogic.HiddenTrigger)
 				{
 				case false:
 					PlayerLogic.HiddenTrigger=true;
-					player.transform.position = ObjectToHide.transform.position;
-					/*foreach(BoxCollider2D a in colliderTriggers)
-					{
-						a.collider2D.enabled=true;
-					}*/
 					ObjectToHide.gameObject.renderer.sortingLayerID=4;
 					SpotLight.gameObject.SetActive(false);
 					speedtemp=PlatformerCharacter2D.walkSpeed;
 					PlatformerCharacter2D.walkSpeed = 0f;
+					EnemyBoxC.enabled=false;
+					colliderTriggerCircle.enabled=false;
+					kleber=true;
 					break;
 				case true:
 					PlayerLogic.HiddenTrigger=false;
 					player.transform.position = ObjectToHide.transform.position;
-					/*foreach(BoxCollider2D a in colliderTriggers)
-					{
-						a.collider2D.enabled=false;
-					}*/
 					ObjectToHide.gameObject.renderer.sortingLayerID=0;
 					SpotLight.gameObject.SetActive(true);
 					PlatformerCharacter2D.walkSpeed=speedtemp;
+					EnemyBoxC.enabled=true;
+					colliderTriggerCircle.enabled=true;
+					kleber=false;
 					break;
 				}
 			}
