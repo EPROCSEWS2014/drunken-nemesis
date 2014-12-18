a67c9d2144a8e268dbf07365781a09b835974643
wbernhs2 <waldemarbernhardt@mail.de>
2014-12-18 20:42:42 +0100

    Neuer Tür-Sound/Pause
    Pause mit 'P'

---
+++ unity/Assets/EPRO_made_Scripts/Pause.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pause : MonoBehaviour {
+
+	// Use this for initialization
+    private bool isPause = false;
+    private Rect butRect;
+
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.P))
+            ToggleTime();
+	}
+
+    void ToggleTime()
+    {
+        if (!isPause)
+        {
+            Time.timeScale = 0;
+        }
+        else
+        {
+            Time.timeScale = 1;
+        }
+        isPause = !isPause;
+    }
+}
